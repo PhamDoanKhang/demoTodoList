7c3dab2351d54a87c1a29d9c23e9edbaccb35185
khang <khangb1809355@student.ctu.edu.vn>
2023-02-15 16:38:53 +0700

    fix

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Todos from "./component/todos"
+function App() {
+  return (
+    <div className="App">
+      <Todos />
+    </div>
+  );
+}
+
+export default App;
